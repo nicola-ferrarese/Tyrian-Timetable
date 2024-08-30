7551ee6d71a9a91f2f482f1be6ff42e5de40b865
nicola-ferrarese <f.nicola98@gmail.com>
2024-08-31 01:08:57 +0200

    [UI] created simple app to visualize responses

--- src/main/scala/com/transportapp/presentation/TransportApp.scala
+++ src/main/scala/com/transportapp/presentation/TransportApp.scala
@@ -8,39 +8,62 @@ import cats.effect.IO
 import com.transportapp.infrastructure.facades.TransportFacade
 
 enum Msg:
-  case Increment, Decrement, None
- 
-case class Model(count: Int)
+  case Departures, Station, None
+  case UpdateOutput(output: String)
+
+case class Model(count: Int, output: String):
+  val transportFacade = TransportFacade()
+
 
 object Model:
-  val initial: Model = Model(0)
+  val initial: Model = Model(0, "")
   
 @JSExportTopLevel("TyrianApp")
 object TransportApp extends TyrianIOApp[Msg, Model]:
-    
+
+
 
   def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) =
-    (Model(0), Cmd.None)
+    (Model(0, ""), Cmd.None)
 
   def update(model: Model): Msg => (Model, Cmd[IO, Msg]) =
-      case Msg.Increment => (model.copy(count = model.count + 1), Cmd.None)
-      case Msg.Decrement => (model.copy(count = model.count - 1), Cmd.Run {
-        TransportFacade().stations.map {
-          case Right(stations) => println(stations); Msg.None
-          case Left(error) => println(s"Error: $error"); Msg.None
+    case Msg.Departures =>
+      (model.copy(count = model.count + 1), Cmd.Run {
+        println("Fetching departures")
+        model.transportFacade.getSLDepartures("1183").map {
+          case Right(departures) => Msg.UpdateOutput(departures.toString())
+          case Left(error) => Msg.UpdateOutput(s"Error: $error")
         }
       })
-      case Msg.None => (model, Cmd.None)
+    case Msg.Station =>
+      (model.copy(count = model.count - 1), Cmd.Run {
+        println("Fetching stations")
+        model.transportFacade.stations.map {
+          case Right(stations) => Msg.UpdateOutput(stations.toString())
+          case Left(error) => Msg.UpdateOutput(s"Error: $error")
+        }
+      })
+    case Msg.UpdateOutput(newOutput) =>
+      (model.copy(output = newOutput), Cmd.None)
+    case Msg.None => (model, Cmd.None)
         
   
-  def router: Location => Msg = _ => Msg.Increment
+  def router: Location => Msg = _ => Msg.None
     
   def view(model: Model): Html[Msg] =
-    div(
-      button(onClick(Msg.Decrement))("-"),
-      div(model.count.toString),
-      button(onClick(Msg.Increment))("+") ,
-      button(onClick(Msg.Increment))("+")
+    div(style("display", "flex"))(
+      div(style("flex", "1"))(
+        button(onClick(Msg.Station))("Stations"),
+        div(model.count.toString),
+        button(onClick(Msg.Departures))("Departures")
+      ),
+      div(style("flex", "1"),
+        style("border-left", "1px solid black"),
+        style("padding-left", "20px"),
+        style("white-space", "pre-wrap"))(
+        h3("Output:"),
+        div(model.output)
+      )
     )
 
   def subscriptions(model: Model): Sub[IO, Msg] =
